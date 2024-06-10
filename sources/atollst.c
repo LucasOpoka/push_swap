f5cd2133b7955e3d8d50d50e1907ec9781423337
Lucas Opoka <lucasopoka@icloud.com>
2024-06-10 20:52:17 +0300

    Working prototype of av validator / number counter / avtoarr

--- sources/atollst.c
+++ sources/atollst.c
@@ -6,35 +6,42 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 13:36:40 by lopoka            #+#    #+#             */
-/*   Updated: 2024/06/10 15:16:33 by lopoka           ###   ########.fr       */
+/*   Updated: 2024/06/10 20:50:50 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
 #include <stdio.h>
 
-t_node	*ft_atollst(int ac, char **av)
+t_node	*ft_atollst(char **av, int i)
 {
-	int	i;
-	
+	int	j;
 
-	if (ac == 2)
+	i--;
+	while (i > 0)
 	{
-		i = ft_strlen(av[1]) - 1;
-		while (i >= 0)
+		j = ft_strlen(av[i]) - 1;
+		while (j >= 0)
 		{
-			while (i > 0 && av[1][i] != ' ')
-				i--;	
-			ft_printf("%d\n", ft_atoi(&av[1][i]));
-			while (av[1][i] == ' ')
-				i--;
+			while (j >= 0 && av[i][j] != ' ')
+			{
+				if (!ft_isdigit(av[i][j]) && av[i][j] != '-' && av[i][j] != '+')
+				{
+					ft_printf_fd(2, "Invalid number passed as argument\n");
+					exit (1);
+				}
+				j--;
+			}
+			ft_printf("%d\n", ft_atoi(&av[i][j + 1]));
+			while (j >= 0 && av[i][j] == ' ')
+				j--;
 		}
+		i--;
 	}
-	return (0);
-	}
-
-int main(int ac, char **av)
-{
-	ft_atollst(ac, av);
 	return (0);
 }
 
+int	main(int ac, char **av)
+{
+	ft_atollst(av, ac);
+	return (0);
+}
