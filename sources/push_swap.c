cbda6a07addc5eaac0d1a5bd15bc54991c21ec09
lopoka <lopoka@student.hive.fi>
2024-06-14 16:44:53 +0300

    Fixed no args error

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 13:36:37 by lopoka            #+#    #+#             */
-/*   Updated: 2024/06/14 15:54:34 by lopoka           ###   ########.fr       */
+/*   Updated: 2024/06/14 16:42:19 by lopoka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -65,6 +65,8 @@ int	main(int ac, char **av)
 	t_stack	a;
 	t_stack	b;
 
+	if (ac < 2)
+		exit(1);
 	ft_init_both_stacks(&a, &b, ac, av);
 	ft_push_to_b(&a, &b);
 	if (a.end == 3)
